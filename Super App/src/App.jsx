c3e2e2ee52b955df990393c890e58b8949c07e21
vennela-024 <vennelaamedaboina@gmail.com>
2024-10-19 20:02:04 +0530

    imported multimedia assets

--- Super App/src/App.jsx
+++ Super App/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RegisterPage />} />
-        //www.abc-xyz.com
-        
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/genres" element={<RegisterPage />} />
+        <Route path="/carousel" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<RegisterPage />} />
+        <Route path="/movies" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
     
