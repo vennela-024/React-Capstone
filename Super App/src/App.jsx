74308ef5b6f1838c857bd33397544e46aa31ac6a
vennela-024 <vennelaamedaboina@gmail.com>
2024-10-19 19:10:58 +0530

    implemented routing

--- Super App/src/App.jsx
+++ Super App/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return 
-    <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RegisterPage />} />
+        //www.abc-xyz.com
+        
+      </Routes>
+    </BrowserRouter>
+    
+  )
 }
 
-export default App
+export default App;
